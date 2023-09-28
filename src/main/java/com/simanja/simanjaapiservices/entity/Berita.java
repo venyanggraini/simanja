9d13d40394db4d2fc4524797b0323c4acbc18d0a
venyanggraini <veny.anggraini93@gmail.com>
2023-09-28 12:44:50 +0700

    add auto publish date

--- src/main/java/com/simanja/simanjaapiservices/entity/Berita.java
+++ src/main/java/com/simanja/simanjaapiservices/entity/Berita.java
@@ -2,10 +2,15 @@ package com.simanja.simanjaapiservices.entity;
 
 import java.util.Date;
 
+import org.hibernate.annotations.CreationTimestamp;
+
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -15,11 +20,17 @@ public class Berita {
     
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "ID_BERITA")
     private Long id;
 
+    @Column(name = "TITLE_BERITA")
     private String title;
-    
+
+    @Column(name = "CONTENT_BERITA")
     private String content;
     
+    @Column(name = "DATE_PUBLISH", columnDefinition = "TIMESTAMP DEFAULT CURRENT_TIMESTAMP")
+    @CreationTimestamp
+    @Temporal(TemporalType.TIMESTAMP)
     private Date publish;
 }
